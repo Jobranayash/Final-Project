1027f224f8a67ff41997f4a05a56dec4f813ea01
Jobranayash <jojo.adel1999@gmail.com>
2025-06-18 23:44:43 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,19 +4,19 @@ const peopleList = document.getElementById("peopleList");
 
 let people = [
     {
-        image: "./Images/add.png",
+        image: "./images/add.png",
         name: "John Doe",
         phone: "123-456-7890",
         address: "123 Nazareth St, Nazareth",
     },
     {
-        image: "./Images/add.png",
+        image: "./images/add.png",
         name: "Jane Smith",
         phone: "987-654-3210",
         address: "321 Raineh , St Ahleh",
     },
     {
-        image: "./Images/add.png",
+        image: "./images/add.png",
         name: "Joo Jarbone",
         phone: "054-654-0399",
         address: "456 Der Hannah, salah"
@@ -31,9 +31,9 @@ people.forEach((person, index) => {
             <img src="${person.image}" alt="img">
         </li>
         <li class="name">${person.name}</li>
-        <li class="btns"><button class="info-btn"><img class="btn" src="./Images/info.png" alt="info"></button>
-                    <button class="edit-btn"><img class="btn" src="./Images/edit.png" alt="edit"></button>
-                    <button class="delete-btn"><img class="btn" src="./Images/delete.png" alt="delete">
+        <li class="btns"><button class="info-btn"><img class="btn" src="./images/info.png" alt="info"></button>
+                    <button class="edit-btn"><img class="btn" src="./images/edit.png" alt="edit"></button>
+                    <button class="delete-btn"><img class="btn" src="./images/delete.png" alt="delete">
                 </li>
     `;
     peopleList.appendChild(personItem);
